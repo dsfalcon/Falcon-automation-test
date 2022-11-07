3cd4a66ec9e17167a1a869759af417fe23adfeba
gourav.sharma <my.somemails@gmail.com>
2022-11-07 20:02:01 +0530

    Card data writing is excel is done.

--- src/main/java/dart/pageEvents/SimulatorTransactionEvents.java
+++ src/main/java/dart/pageEvents/SimulatorTransactionEvents.java
@@ -25,9 +25,8 @@ public class SimulatorTransactionEvents extends SimulatorTransactionElements {
 
     }
 
-    public void OpenSimulator(String cardIDs) throws IOException, InterruptedException {
+    public void OpenSimulator(String cardIDs) throws InterruptedException {
         driver.get("http://simulator.qa.kitetest.me/");
-        dbQuery(DatabaseConnection.connectDB());
         Thread.sleep(1000);
         cardID.sendKeys(cardIDs);
         merchantName.click();
@@ -36,41 +35,8 @@ public class SimulatorTransactionEvents extends SimulatorTransactionElements {
         ECOM.click();
         Amount.sendKeys("101");
         submitButton.click();
-        Thread.sleep(2000);
+        Thread.sleep(4000);
 
     }
-
-    public void dbQuery(Connection con) throws IOException {
-
-        Statement smt = null;
-        String path=System.getProperty("user.dir")+"/datafiles/"+"cardData.xlsx";
-        try {
-            smt = con.createStatement();
-            ResultSet rs = smt.executeQuery("SELECT * from kite_card.card where enterprise_id='lalitgift15' and card_status='ACTIVE' and online_channel_status='ENABLED' and pos_channel_status='ENABLED'");
-            // ResultSet rs = smt.executeQuery(dbQuery);
-
-
-            int count = 0;
-            while (rs.next()) {
-                count++;
-                System.out.println("Count of row "+count);
-                System.out.print(rs.getString(1));
-                XLUtils.setCellData(path,"Sheet1",count,0, rs.getString(1));
-                System.out.println();
-
-
-
-                //* ****************** Set Data in Excel***************************** *//
-            }
-//
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-
-
-    }
-
 
 }
